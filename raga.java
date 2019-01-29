b090afeea1cd81f0adead4af4ef2d4b52f277fef
Anvay Mishra <44741228+anvaymishra@users.noreply.github.com>
2019-01-29 11:34:26 +0530

    Add files via upload
    added a java file.

---
+++ raga.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class raga
+{
+public static void main(String args[])
+    {
+     Scanner alu=new Scanner(System.in);
+     String machine=alu.nextLine();
+     if(machine.equals("Aloo")||machine.equals("aloo"))
+      {
+       System.out.println("Sona"); 
+        }
+        else
+        {
+         System.out.println("Acche din aa gaye");
+        }
+    }
+}
